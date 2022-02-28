903e5412438882b4a2d6da3374cfc04223756c6f
Snow Memeurr <snowthefox101@gmail.com>
2022-02-27 23:51:16 -0500

    Edited code so that arrays and lists are no longer directly hardcoded. 2/27/2022.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 /**
-* 2/13/2022
+* 2/27/2022
 * CSC 153
 * Ciara McLaughlin
-* Text based adventure game with only a menu and movement system.
+* Text based adventure game with only a menu and movement system. Also creates player text file.
 */
 
 namespace ConsoleUI
@@ -17,143 +18,228 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
 
-            bool exit = false;
-            int roomLocation = 0;
-
-            //Arrays
-            string[] rooms = new string[5] { "Entrance", "Hallway", "Bedroom", "Living Room", "Backyard" };
-            string[] weapons = new string[4] { "Ghost Blaster", "Crucifix", "Incense Stick", "Salt Shotgun" };
-            string[] potion = new string[2] { "Holy Water", "Herbal Brew" };
-            string[] treasure = new string[3] { "Ancient Relic", "Purified Armor", "Ancient Ambulet" };
-            //Lists
-
-            List<string> items = new List<string>() { "Prayer beads", "Religious Text", "Holy Statue", "Rosemary" };
-            List<string> mobs = new List<string>() { "Lower Ghost", "Stronger Ghost", "Poltergeist ", "Yokai", "Banshee" };
-
-            while (exit == false)
+            try
             {
-                Console.WriteLine($"You are in - {rooms[roomLocation]} ");
-                Console.WriteLine("1. Display Rooms ");
-                Console.WriteLine("2. Display Weapon ");
-                Console.WriteLine("3. Display Potion");
-                Console.WriteLine("4. Display Treasure ");
-                Console.WriteLine("5. Display Items ");
-                Console.WriteLine("6. Display Mobs ");
-                Console.WriteLine("7. Exit");
-                Console.WriteLine("Enter a choice > ");
-                string input = Console.ReadLine();
 
-                switch (input.ToLower())
+                StreamWriter outputFile;
+
+                Console.WriteLine("Enter your name > ");
+                string playerName = Console.ReadLine();
+
+                outputFile = File.AppendText("PlayerInfo.txt");
+
+                Console.WriteLine("Enter your class > ");
+                string playerClass = Console.ReadLine()
+                Console.WriteLine("Enter your weapon > ");
+                string playerWeapon = Console.ReadLine();
+
+                outputFile.WriteLine($"{playerName},{playerClass},{playerWeapon}");
+
+                outputFile.Close();
+
+                int roomLocation = 0; 
+                bool exit = false;
+
+                StreamReader inputFileItem = File.OpenText("items.txt");
+                StreamReader inputFileMob = File.OpenText("Mobs.txt");
+                StreamReader inputFilePotion = File.OpenText("potions.txt");
+                StreamReader inputFileRoom = File.OpenText("rooms.txt");
+                StreamReader inputFileTreasure = File.OpenText("treasure.txt");
+                StreamReader inputFileWeapon = File.OpenText("weapons.txt");
+
+
+                List<string> items = new List<string>();
+                List<string> mobs = new List<string>();
+                List<string> rooms = new List<string>();
+                List<string> weapons = new List<string>();
+                List<string> potions = new List<string>();
+                List<string> treasures = new List<string>();
+
+
+
+                while (exit == false)
                 {
-                    case "1":
-                        foreach (string value in rooms)
-                        {
-                            Console.WriteLine(value);
-                        }
-                        Console.WriteLine();
-                        break;
-                    case "2":
-                        {
-                            Array.Sort(weapons);
+                    while (inputFileRoom.EndOfStream == false)
+                    {
+                        rooms.Add(inputFileRoom.ReadLine());
+                    }
+                    Console.WriteLine($"You are in - {rooms[roomLocation]} "); 
+                    Console.WriteLine("1. Display Rooms ");
+                    Console.WriteLine("2. Display Weapon ");
+                    Console.WriteLine("3. Display Potion");
+                    Console.WriteLine("4. Display Treasure ");
+                    Console.WriteLine("5. Display Items ");
+                    Console.WriteLine("6. Display Mobs ");
+                    Console.WriteLine("7. Exit");
+                    Console.WriteLine("Enter a choice > ");
+                    string input = Console.ReadLine();
+                    Console.WriteLine(); 
 
-                            Array.ForEach<string>(weapons, s => Console.WriteLine(s));
-                        }
-                        break;
-                    case "3":
-                        foreach (string value in potion)
-                        {
-                            Console.WriteLine(value);
+                    
+                    switch (input.ToLower())
+                    {
+                        case "1":
+                            while (inputFileRoom.EndOfStream == false)
+                            {
+                                rooms.Add(inputFileRoom.ReadLine());
+                            }
+                            foreach (string room in rooms)
+                            {
+                                Console.Write($"{room} ");
+                            }
                             Console.WriteLine();
-                        }
-                        break;
-                    case "4":
-                        foreach (string value in treasure)
-                        {
-                            Console.WriteLine(value);
+                            break;
+                        case "2":
+                            while (inputFileWeapon.EndOfStream == false)
+                            {
+                                weapons.Add(inputFileWeapon.ReadLine());
+                            }
+                            foreach (string weapon in weapons)
+                            {
+                                Console.Write($"{weapon} ");
+                                Console.WriteLine();
+                            }
+                            break;
+                        case "3":
+                            while (inputFilePotion.EndOfStream == false)
+                            {
+                                potions.Add(inputFilePotion.ReadLine());
+                            }
+                            foreach (string potion in potions)
+                            {
+                                Console.Write($"{potion} ");
+                                Console.WriteLine();
+                            }
+                            break;
+                        case "4":
+                            while (inputFileTreasure.EndOfStream == false)
+                            {
+                                treasures.Add(inputFileTreasure.ReadLine());
+                            }
+                            foreach (string treasure in treasures)
+                            {
+                                Console.Write($"{treasure} ");
+                                Console.WriteLine();
+                            }
+                            break;
+                        case "5":
+                            while (inputFileItem.EndOfStream == false)
+                            {
+                                items.Add(inputFileItem.ReadLine());
+                            }
+                            foreach (string item in items)
+                            {
+                                Console.Write($"{item} ");
+                                Console.WriteLine();
+                            }
+                            break;
+                        case "6":
+                            while (inputFileMob.EndOfStream == false)
+                            {
+                                mobs.Add(inputFileMob.ReadLine());
+                            }
+                            foreach (string mob in mobs)
+                            {
+                                Console.Write($"{mob} ");
+                                Console.WriteLine();
+                            }
+                            break;
+                        case "rooms":
+                            while (inputFileRoom.EndOfStream == false)
+                            {
+                                rooms.Add(inputFileRoom.ReadLine());
+                            }
+                            foreach (string room in rooms)
+                            {
+                                Console.Write($"{room} ");
+                                Console.WriteLine();
+                            }
+                            break;
+                        case "weapons":
+                            while (inputFileWeapon.EndOfStream == false)
+                            {
+                                weapons.Add(inputFileWeapon.ReadLine());
+                            }
+                            foreach (string weapon in weapons)
+                            {
+                                Console.Write($"{weapon} ");
+                                Console.WriteLine();
+                            }
+                            break;
+                        case "potions":
+                            while (inputFilePotion.EndOfStream == false)
+                            {
+                                potions.Add(inputFilePotion.ReadLine());
+                            }
+                            foreach (string potion in potions)
+                            {
+                                Console.Write($"{potion} ");
+                                Console.WriteLine();
+                            }
+                            break;
+                        case "treasure":
+                            while (inputFileTreasure.EndOfStream == false)
+                            {
+                                treasures.Add(inputFileTreasure.ReadLine());
+                            }
+                            foreach (string treasure in treasures)
+                            {
+                                Console.Write($"{treasure} ");
+                                Console.WriteLine();
+                            }
+                            break;
+                        case "items":
+                            while (inputFileItem.EndOfStream == false)
+                            {
+                                items.Add(inputFileItem.ReadLine());
+                            }
+                            foreach (string item in items)
+                            {
+                                Console.Write($"{item} ");
+                                Console.WriteLine();
+                            }
+                            break;
+                        case "n":
+                            if (roomLocation != 4)
+                            {
+                                roomLocation++;
+                            }
+                            else
+                            {
+                                Console.WriteLine("You can not go north!");
+                                Console.WriteLine();
+                            }
+                            break;
+                        case "s":
+                            if (roomLocation != 0)
+                            {
+                                roomLocation--;
+                            }
+                            else
+                            {
+                                Console.WriteLine("You can not go south!");
+                                Console.WriteLine();
+                            }
+                            break;
+                        case "7":
+                            exit = true;
+                            break;
+                        default:
+                            Console.WriteLine("Not a choice.");
                             Console.WriteLine();
-                        }
-                        break;
-                    case "5":
-                        foreach (string value in items)
-                        {
-                            Console.WriteLine(value);
-                            Console.WriteLine();
-                        }
-                        break;
-                    case "6":
-                        foreach (string value in mobs)
-                        {
-                            Console.WriteLine(value);
-                            Console.WriteLine();
-                        }
-                        break;
-                    case "rooms":
-                        foreach (string value in rooms)
-                        {
-                            Console.WriteLine(value);
-                            Console.WriteLine();
-                        }
-                        break;
-                    case "weapons":
-                        foreach (string value in weapons)
-                        {
-                            Console.WriteLine(value);
-                            Console.WriteLine();
-                        }
-                        break;
-                    case "potions":
-                        foreach (string value in potion)
-                        {
-                            Console.WriteLine(value);
-                            Console.WriteLine();
-                        }
-                        break;
-                    case "treasure":
-                        foreach (string value in treasure)
-                        {
-                            Console.WriteLine(value);
-                            Console.WriteLine();
-                        }
-                        break;
-                    case "items":
-                        foreach (string value in items)
-                        {
-                            Console.WriteLine(value);
-                            Console.WriteLine();
-                        }
-                        break;
-                    case "n":
-                        if (roomLocation != 4)
-                        {
-                            roomLocation++;
-                        }
-                        else
-                        {
-                            Console.WriteLine("You can not go north!");
-                            Console.WriteLine();
-                        }
-                        break;
-                    case "s":
-                        if (roomLocation != 0)
-                        {
-                            roomLocation--;
-                        }
-                        else
-                        {
-                            Console.WriteLine("You can not go south!");
-                            Console.WriteLine();
-                        }
-                        break;
-                    case "7":
-                        exit = true;
-                        break;
-                    default:
-                        Console.WriteLine("Not a choice.");
-                        Console.WriteLine();
-                        break;
+                            break;
+                    }
 
                 }
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+
+
         }
     }
-}
+    }
+
