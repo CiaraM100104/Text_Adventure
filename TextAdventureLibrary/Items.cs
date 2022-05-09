1984b64abb4bde9e26006ad0360fcc6d7b6f24fb
Snow Memeurr <snowthefox101@gmail.com>
2022-05-08 23:32:38 -0400

    added items to inherit off of ingameitems as well since I accidentally forgot.

--- TextAdventureLibrary/Items.cs
+++ TextAdventureLibrary/Items.cs
@@ -6,49 +6,23 @@ using System.Threading.Tasks;
 
 namespace TextAdventureLibrary
 {
-    public class Items
+    public class Items : IngameItem
     {
-        private string _idNumber;
-        private string _name;
-        private string _price;
+
         private string _questItem;
         private string _required;
-        private string _description;
+ 
 
 
         public Items(string idNumber, string name, string price, string questItem, string required, string description)
+            : base(name, idNumber, price, description)
         {
-            name = _name;
-            idNumber = _idNumber;
-            price = _price;
+ 
             questItem = _questItem;
             required = _required;
-            description = _description;
+  
         }
 
-        public string idNumber
-        {
-            get { return _idNumber; }
-            set { }
-        }
-
-        public string name
-        {
-            get { return _name; }
-            set { }
-        }
-
-        public string description
-        {
-            get { return _description; }
-            set { }
-        }
-
-        public string price
-        {
-            get { return _price; }
-            set { }
-        }
 
         public string questItem
         {
